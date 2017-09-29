794870e5cae03d9c0166cea1dcf4599710e9e30b
Aarron Lee <aclee91@gmail.com>
2017-09-28 21:51:26 -0400

    basic recursive quicksort implemented

---
+++ quicksort.rb
@@ -0,0 +1,17 @@
+
+
+def quicksort(arr)
+  return arr if arr.length <=1
+
+  pivot = arr.first
+  left = arr[1..-1].select{|num| num < pivot}
+  right = arr[1..-1].select{|num| num >= pivot}
+
+  quicksort(left) + [pivot] + quicksort(right)
+end
+
+
+
+arr = [0..10000].shuffle
+
+puts quicksort(arr) == arr.sort
